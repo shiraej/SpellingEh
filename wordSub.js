8f57ef4b05a0e4ba02f59b0625d7f0eb2d7f1f6b
Shira <shiraej@gmail.com>
2021-08-12 14:27:06 -0700

    figured out how to have the text sub access all of the text nodes

--- wordSub.js
+++ wordSub.js
@@ -1,4 +1,5 @@
-import {dictionary} from './wordMap.js';
+
+import {dictionary} from './wordDict.js';
 //loop through a body of text and substitue any words found in the dictionary
 
 const wordSubber = function (bodyoftext, dict = dictionary) {
@@ -13,11 +14,15 @@ const wordSubber = function (bodyoftext, dict = dictionary) {
 	return wordsArr.join(' ');
 }
 
-const pageHandler = function(){
-	let page = document.querySelectorAll("body");
-	for (let ele of page) {
-		eleText = ele.innerText;
-		subEleText = wordSubber(eleText);
-		ele.innerText = subEleText;
+const findandsub = function (nodeParent){
+
+	if (nodeParent.nodeType === Node.TEXT_NODE) {
+		nodeParent.textContent = wordSubber(nodeParent.textContent);
+	}
+	else {
+		for (let child of nodeParent.childNodes){
+				findandsub(child);
+		}
 	}
 }
+findandsub(document.body);
