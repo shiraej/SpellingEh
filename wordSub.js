e7f5741dd62e911b6d40f34473fb3ae6a1f54b8b
Shira <shiraej@gmail.com>
2021-08-18 14:21:40 -0700

    changed icon; added comments to wordSub and cleaned up variable names

--- wordSub.js
+++ wordSub.js
@@ -1,19 +1,19 @@
-//to fix: handle ed, s, er, ing endings
-//loop through a body of text and substitue any words found in the dictionary
+/*meat and potatoes of the extension... handles the search through all text nodes, matching to words
+ in the dictionary and handles common variations like a symbol or an "s" at the end of the word*/
 
+//icon: By Vincent Le Moign, CC BY 4.0, https://commons.wikimedia.org/w/index.php?curid=68652390
 
 const handleUpperCase = function(word){
+	//if first letter is an upper case, this function will return the word (repl) with altered spelling and case preserved
 	let repl = dictionary[word.toLowerCase()];
-	for (let i=0;i< word.length;i++){
-		if (word[i] == word[i].toUpperCase()) {
-			repl = repl.slice(0,i)+repl[i].toUpperCase()+repl.slice(i+1,repl.length);
-		}
+	if (word[0] == word[0].toUpperCase()){
+		repl = repl[0].toUpperCase() + repl.slice(1,repl.length)
 	}
 	return repl;
 }
 
 const makeRepl = function(word, char, ind){
-
+	//handles the logic for making the replacement word (repl) checking for symbols or 's' at the end of the input word
 	if (char.test(word[ind])){
 		let nosymb = word.slice(0,ind)
 		let sChar = /s/;	//check for s at end before symbol
@@ -39,15 +39,17 @@ const makeRepl = function(word, char, ind){
 	}
 }
 
-const wordSubber = function (bodyoftext, dict = dictionary) {
+const wordSubber = function (textNode, dict = dictionary) {
+	//handles the search through all words in an inputted text node
+	
 	//1. split the body of text into a word array
-	let wordsArr = bodyoftext.split(' ');
+	let wordsArr = textNode.split(' ');
     let symbChar = /[$-/:-?{-~!"^_`\[\]s]/;
 	//2. loop through these words
 	for (let word of wordsArr) {
-		//check for symbol at end of word
 		let lastInd = word.length-1;
-		let repl = makeRepl(word,symbChar, lastInd);
+		//3. use makeRepl to make the replacement word if it's in the word Dictionary
+		let repl = makeRepl(word,symbChar,lastInd);
 		if (repl){
 			wordsArr[wordsArr.indexOf(word)] = repl;
 		}
@@ -57,7 +59,7 @@ const wordSubber = function (bodyoftext, dict = dictionary) {
 }
 
 const findandsub = function (nodeParent){
-
+//recursively finds all text nodes in a document and applies the wordSubber function to each.
 	if (nodeParent.nodeType === Node.TEXT_NODE) {
 		nodeParent.textContent = wordSubber(nodeParent.textContent);
 	}
